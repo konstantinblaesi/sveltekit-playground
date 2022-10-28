6f5aa081d6d9705989065c5c6b774a475e10afe9
Konstantin Bläsi <kblaesi@gmail.com>
2022-10-28 23:44:54 +0200

    Disable sveltekit hash scroll, do smooth scroll after navigate.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to SvelteKit</h1>
+<a href="/example#link" data-sveltekit-noscroll>Go to example</a>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
